f7ec79f0c0d7a88bc67d571a22141768ffd5fb60
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2019-11-01 11:48:21 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  siteTitle: 'Gatsby Starter Eventually', // <title>
+  manifestName: 'Eventually',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
+  // social
+  heading: 'Alex Panteli',
+  subHeading:
+    "I'm Alex Panteli a Frontend Web Developer based in Limassol, Cyprus. I'm a mid level, professional and friendly digital specialist focusing on Frontend Web Development. A meticulous problem solver, I approach all tasks with energy, creativity and passion. I take pride in a job well done and enjoy working with like-minded, passionate individuals.",
+  socialLinks: [
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:alekspanteli@gmail.com',
+    },
+  ],
+};
