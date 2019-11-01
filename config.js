98c1f9bda8e9df39fd7c0f7b88f4ddb89c24e210
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2019-11-01 12:40:40 +0200

    site title change

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteTitle: 'Alex Panteli - Frontend Developer', // <title>
   manifestName: 'Alex Panteli',
-  manifestShortName: 'Landing', // max 12 characters
+  manifestShortName: '', // max 12 characters
   manifestStartUrl: '/',
   manifestBackgroundColor: '#663399',
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifestIcon: '',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
   heading: 'Alex Panteli',
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,8 +38,8 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Eventually' },
-                { name: 'keywords', content: 'site, web' },
+                { name: 'description', content: '' },
+                { name: 'keywords', content: '' },
               ]}
             >
               <html lang="en" />
